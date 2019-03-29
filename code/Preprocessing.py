b1382432e7fd0959a37e3d74c4712196aada930f
Siyang Liu <liusiyang@gwu.edu>
2019-03-29 12:30:03 -0400

    save book dictionary to Json file

--- code/Preprocessing.py
+++ code/Preprocessing.py
@@ -6,38 +6,49 @@ from nltk.corpus import stopwords
 import chardet
 import re
 import os
+import json
+from pathlib import Path
 def main():
     dir = "../text_data/"
     raw_text_data = read_file(dir)
     processed_text_data = preprocessing(raw_text_data)
-  
+    print(processed_text_data['Harry Potter and the Prisoner of Azkaban'])
 
 def read_file(dir):
-    files = []
+    json_file = Path('{}Harry-Potter-Series.json'.format(dir))
     book = {}
-    pattern = '\.txt'
-    for f in os.listdir(dir):
-        if re.search(pattern,f):
-            files.append(f)
-    pattern = '(.+)\..+$'
-    for file_name in files:
-        decode_text = []
-        with open(dir+file_name, 'rb') as f_read:
-            text = f_read.readlines()
-            for line in text:
-                type = chardet.detect(line)
-                line = line.decode(type["encoding"])
-                decode_text.append(line)
-            file_name = re.findall(pattern,file_name)[0]
-            book[file_name] = decode_text
-        f_read.close()
+    if not json_file.exists():
+        files = []
+        pattern = '\.txt'
+        for readfile in os.listdir(dir):
+            if re.search(pattern, readfile):
+                files.append(readfile)
+        pattern = '(.+)\..+$'
+        for file_name in files:
+            decode_text = []
+            with open(dir + file_name, 'rb') as f_read:
+                text = f_read.readlines()
+                for line in text:
+                    type = chardet.detect(line)
+                    line = line.decode(type["encoding"])
+                    decode_text.append(line)
+                file_name = re.findall(pattern, file_name)[0]
+                book[file_name] = decode_text
+            f_read.close()
+
+        ##TODO:Save book to json file
+        with open(dir + 'Harry-Potter-Series.json', 'a') as outfile:
+            json.dump(book, outfile, ensure_ascii=False)
+            outfile.write('\n')
+    else:
+        readfile = open('{}Harry-Potter-Series.json'.format(dir),"r")
+        book = eval(readfile.read())
     return book
 
 def preprocessing(data):
     processed_data = {}
     for key in data.keys():
         text_of_book = data[key]
-        # sentences = sent_tokenize(text_of_book)
         pattern = r'^[¡]*'
         new_sentence = []
         for sentence in text_of_book:
