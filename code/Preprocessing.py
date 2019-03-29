cfcad85225ebd7bbd4ed67e5c57282963ca291c8
Siyang Liu <liusiyang@gwu.edu>
2019-03-29 19:00:36 -0400

    Stemming and lemmatization with NLTK

--- code/Preprocessing.py
+++ code/Preprocessing.py
@@ -3,19 +3,20 @@ from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 import chardet
 import re
 import os
 import json
 from pathlib import Path
 def main():
-    dir = "../text_data/"
+    dir = "../NLP-project/text_data/"
     raw_text_data = read_file(dir)
     processed_text_data = preprocessing(raw_text_data)
     print(processed_text_data['Harry Potter and the Prisoner of Azkaban'])
 
 def read_file(dir):
-    json_file = Path('{}Harry-Potter-Series.json'.format(dir))
+    json_file = Path('{}Raw-Harry-Potter-Series.json'.format(dir))
     book = {}
     if not json_file.exists():
         files = []
@@ -37,11 +38,11 @@ def read_file(dir):
             f_read.close()
 
         ##TODO:Save book to json file
-        with open(dir + 'Harry-Potter-Series.json', 'a') as outfile:
+        with open(dir + 'Raw-Harry-Potter-Series.json', 'a') as outfile:
             json.dump(book, outfile, ensure_ascii=False)
             outfile.write('\n')
     else:
-        readfile = open('{}Harry-Potter-Series.json'.format(dir),"r")
+        readfile = open('{}Raw-Harry-Potter-Series.json'.format(dir),"r")
         book = eval(readfile.read())
     return book
 
@@ -55,11 +56,37 @@ def preprocessing(data):
             sentence = sentence.strip('\n')
             sentence = re.sub(pattern, '', sentence)
             new_sentence.append(sentence)
+
         sentence_word_list = []
+        ##Tokenization
         for sentence in new_sentence:
             sentence_word_list.append(word_tokenize(sentence))
-        processed_data[key] = sentence_word_list
+
+        ##Stemming
+        new_stemming_sentence_word_list = []
+        ps = PorterStemmer()
+
+        for sentence_word in sentence_word_list:
+            stemming_words = []
+            for word in sentence_word:
+                stemming_words.append(ps.stem(word))
+            new_stemming_sentence_word_list.append(stemming_words)
+
+        ##Lemmatization
+        new_lemmatized_sentence_word_list = []
+        lemmatizer = WordNetLemmatizer()
+
+        for sentence_word in new_stemming_sentence_word_list:
+            lemmatized_words = []
+            for word in sentence_word:
+                lemmatized_words.append(lemmatizer.lemmatize(word))
+            new_lemmatized_sentence_word_list.append(lemmatized_words)
+        processed_data[key] = new_lemmatized_sentence_word_list
+
     return processed_data
+
+
+
 
 
 
